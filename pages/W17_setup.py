34c4c5b57a8f22ce9c438326bb4fe2a91a683013
Ruslan <ruslan861125@gmail.com>
2025-03-01 12:33:34 +0100

    v3

--- pages/W17_setup.py
+++ pages/W17_setup.py
@@ -1,17 +1,12 @@
-import subprocess
 import streamlit as st
 
 st.set_page_config(page_title="W17 driftsättning", layout="wide")
 
-
-def open_excel_file(file_path):
-    try:
-        subprocess.call(["open", "-a", "Microsoft Excel", file_path])
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-# Example usage
-file_path = "files/W17_setup.xlsm"
-if st.button("Open Excel File"):
-    open_excel_file(file_path)
+filepath = "files/W17_setup.xlsm"
+with open(filepath, "rb") as file:
+    st.download_button(
+        "Download File",
+        data=file,
+        mime="application/vnd.ms-excel.sheet.macroEnabled.12",
+        file_name="W17_setup.xlsm",
+    )
